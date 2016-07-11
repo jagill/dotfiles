a3851c38cf230d4a2ae8d32271e7ef35fcafc624
James Gill <jamesagill@gmail.com>
2016-07-11 18:34:47 +0000

    Fixing config dir and old psqlrc link.

--- dotfiles_ubuntu.sh
+++ dotfiles_ubuntu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CONFIG_DIR=${CONFIG_DIR:-$HOME/Dropbox/config}
+CONFIG_DIR=${CONFIG_DIR:-$HOME/dotfiles}
 if [ ! -d $CONFIG_DIR ]; then
     echo CONFIG_DIR $CONFIG_DIR is not a valid directory.
     exit 1
@@ -10,7 +10,7 @@ fi
 [ -f $HOME/.bash_prompt ] || ln -s $CONFIG_DIR/bash_prompt $HOME/.bash_prompt
 [ -f $HOME/.ackrc ] || ln -s $CONFIG_DIR/ackrc $HOME/.ackrc
 [ -f $HOME/.ctags ] || ln -s $CONFIG_DIR/ctags $HOME/.ctags
-[ -f $HOME/.psqlrc ] || ln -s $CONFIG_DIR/psqlrc $HOME/.psqlrc
 [ -d $HOME/.vim ] || ln -s $CONFIG_DIR/vim $HOME/.vim
 [ -f $HOME/.vimrc ] || ln -s $HOME/.vim/vimrc $HOME/.vimrc
+[ -f $HOME/.gitconfig ] || ln -s $CONFIG_DIR/gitconfig $HOME/.gitconfig
 [ -f $HOME/.gitignore_global ] || ln -s $CONFIG_DIR/gitignore $HOME/.gitignore_global
