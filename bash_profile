45d72899d5f8076fe053ac375d38c583783edb88
James A. Gill <jagill@fb.com>
2016-10-26 23:09:43 -0700

    Cleaning up some files!

--- bash_profile
+++ bash_profile
@@ -1,5 +1,5 @@
 if [ -f $HOME/.bashrc ]; then
-    source $HOME/.bashrc
+  source $HOME/.bashrc
 fi
 if [ -f $HOME/.projectsrc ]; then
    source $HOME/.projectsrc 
---
+++ dotfiles.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+if [ -z "$1" ]; then
+  echo "Please supply the architecture (macos, ubuntu, rh) as the first argument." >&2
+  exit 1
+fi
+
+arch=$1
+
+if [ ! "$arch" == "macos" -o "$arch" == "ubuntu" -o "$arch" == "rh" ]; then
+  echo "The architecture must be one of macos, ubuntu, rh." >&2
+  exit 1
+fi
+
+
+CONFIG_DIR=${CONFIG_DIR:-$HOME/dotfiles}
+if [ ! -d $CONFIG_DIR ]; then
+  echo CONFIG_DIR $CONFIG_DIR is not a valid directory.
+  exit 1
+fi
+
+[ -f $HOME/.bashrc ] || ln -s $CONFIG_DIR/bashrc_${arch} $HOME/.bashrc
+[ -f $HOME/.gitignore_global ] || ln -s $CONFIG_DIR/gitignore $HOME/.gitignore_global
+
+for f in bash_profile vimrc ackrc ctags gitconfig jshintrc tmux.conf; do
+  [ -f $HOME/.$f ] || ln -s $CONFIG_DIR/$f $HOME/.$f
+done
+
+if [ ! -d $HOME/.vim ]; then
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/vundle
+  vim +PluginInstall +qall
+fi
